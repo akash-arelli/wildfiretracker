8d425341e752c39443a98ebf8365b351ac9b3363
akash-arelli <akasharelli@gmail.com>
2021-01-06 20:19:14 +0530

    wild fire traker

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { Icon } from '@iconify/react'
+import locationIcon from '@iconify/icons-mdi/fire-alert'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1> Wildfire Tracker (Powered By NASA)</h1>
+        </header>
+    )
+}
+
+export default Header
